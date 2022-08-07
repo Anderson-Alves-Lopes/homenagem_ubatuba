448c716b3b14cc905f8615acc3d131d9da24a9d9
Anderson Alves <nenoalveslopes@hotmail.com>
2022-08-07 10:02:25 -0300

    Adicionando section Opções de Lazer

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,17 @@ const menu = document.querySelector('.menu');
 menu.addEventListener('click',function(){
     let ul = document.querySelector('header ul');
  
-    if(ul.style.display === 'none'){
-       ul.style.display = 'flex';
+    if(ul.style.display === 'flex'){
+      ul.style.opacity = '0';
        setTimeout(function(){
-         ul.style.opacity = '1';
+        ul.style.display = 'none';
+         
        },300)
-    }else if(ul.style.display = 'flex'){
-     ul.style.opacity = '0';
+    }else if(ul.style.display = 'none'){
+      ul.style.display = 'flex';
      setTimeout(function(){
-         ul.style.display = 'none';
+       ul.style.opacity = '1';
+         
      },300)
     }
  
